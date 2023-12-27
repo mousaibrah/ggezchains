24d8f38fbc8d47aefc97e9fdb096f7847d700ad5
mousaibrah <mousaKibrahim@gmail.com>
2023-12-27 10:02:40 +0300

    create trade logic

--- x/trade/keeper/msg_server_create_trade.go
+++ x/trade/keeper/msg_server_create_trade.go
@@ -2,16 +2,73 @@ package keeper
 
 import (
 	"context"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/mousaibrah/ggezchain/x/trade/types"
+	"time"
 )
 
 func (k msgServer) CreateTrade(goCtx context.Context, msg *types.MsgCreateTrade) (*types.MsgCreateTradeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	// isValidTradeDataObject , validateTradeDataErr := k.ValidateTradeData(msg.TradeData)
+	// if !isValidTradeDataObject {
+	// 	return nil, validateTradeDataErr
+	// }
+	// isAllowed, _ := k.IsAddressAllowed(k.stakingKeeper, ctx, msg.Creator, types.CreateTrade)
+	// if !isAllowed {
+	// 	//panic("you don't have permission to perform this action")
+	// 	return nil, types.ErrInvalidMakerPermission
+	// }
+	currentTime := time.Now()
+	formattedDate := currentTime.Format("2006-01-02 03:04")
 
-	// TODO: Handling the message
-	_ = ctx
+	// err := msg.Validate()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	return &types.MsgCreateTradeResponse{}, nil
+	tradeIndex, found := k.Keeper.GetTradeIndex(ctx)
+	if !found {
+		panic("Trade Index not found")
+	}
+
+	newIndex := tradeIndex.NextId
+	status := types.Pending
+
+	storedTrade := types.StoredTrade{
+		TradeIndex:      newIndex,
+		Status:          status,
+		CreateDate:      formattedDate,
+		TradeType:       msg.TradeType,
+		Coin:            msg.Coin,
+		Price:           msg.Price,
+		Quantity:        msg.Quantity,
+		ReceiverAddress: msg.ReceiverAddress,
+		Maker:           msg.Creator,
+		Checker:         "",
+		ProcessDate:     formattedDate,
+		TradeData:       msg.TradeData,
+		Result:          types.ErrTradeCreatedSuccessfully.Error(),
+	}
+
+	storedTempTrade := types.StoredTempTrade{
+		TradeIndex:     newIndex,
+		TempTradeIndex: newIndex,
+		CreateDate:     formattedDate,
+	}
+
+	k.Keeper.SetStoredTrade(ctx, storedTrade)
+	k.Keeper.SetStoredTempTrade(ctx, storedTempTrade)
+
+	tradeIndex.NextId++
+
+	k.Keeper.SetTradeIndex(ctx, tradeIndex)
+
+	// k.Keeper.CancelExpiredPendingTrades(ctx)
+	// ctx.EventManager().EmitEvent(
+	// 	sdk.NewEvent(types.CancelExpiredPendingTradesEventType),
+	// )
+	return &types.MsgCreateTradeResponse{
+		TradeIndex: newIndex,
+		Status:     status,
+	}, nil
 }
--- x/trade/types/expected_keepers.go
+++ x/trade/types/expected_keepers.go
@@ -15,7 +15,10 @@ type AccountKeeper interface {
 // BankKeeper defines the expected interface for the Bank module.
 type BankKeeper interface {
 	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
-	// Methods imported from bank should be defined here
+	// MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	// BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	// SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	// SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
 
 // ParamSubspace defines the expected Subspace interface for parameters.
